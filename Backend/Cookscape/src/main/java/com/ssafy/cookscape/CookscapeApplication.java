ec8486bda73ac9a48f220d15a2e924851ba34dca
BinaryHyuk <wlswl2435@naver.com>
2023-02-28 09:30:41 +0900

    Init : 백엔드 프로젝트 생성
    Former-commit-id: 43f2da93dcb032ef53c2f0f689a22be7942acd8e

---
+++ Backend/Cookscape/src/main/java/com/ssafy/cookscape/CookscapeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.cookscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookscapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookscapeApplication.class, args);
+    }
+
+}
---
+++ Backend/Cookscape/src/test/java/com/ssafy/cookscape/CookscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.cookscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookscapeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
